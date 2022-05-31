92a83b2aa7219147431a5c45f095d190931bb5ea
hari1155 <harinocharla@gmail.com>
2022-05-31 12:54:14 +0530

    added sample.java file

---
+++ sample.java
@@ -0,0 +1,13 @@
+# this is my java application
+import java.lang.*;
+
+class sample
+{
+ public static void main(string[] args)
+ {
+  system.output.printin("welcome to java application...");
+  int a=10,b=20,c;
+  c=a+b;
+  system.output.printin("sum of "+a+" and "+b+" is "+c);
+ }
+}
